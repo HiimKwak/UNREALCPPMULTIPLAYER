bedb75857b63a36214d0bd7bd66900df84947269
곽민규 <mindol0320@gmail.com>
2026-05-29 16:57:48 +0900

    NetDriver, NetConnection, Channel

--- Source/ArenaBattle/Game/ABGameMode.cpp
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -4,6 +4,7 @@
 #include "Game/ABGameMode.h"
 #include "ABGameMode.h"
 
+#include "ABGameState.h"
 #include "ArenaBattle.h"
 #include "Player/ABPlayerController.h"
 
@@ -20,6 +21,8 @@ AABGameMode::AABGameMode()
 	{
 		PlayerControllerClass = PlayerControllerClassRef.Class;
 	}
+	
+	GameStateClass = AABGameState::StaticClass();
 }
 
 void AABGameMode::OnPlayerDead()
@@ -30,8 +33,11 @@ void AABGameMode::OnPlayerDead()
 void AABGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
 	FString& ErrorMessage)
 {
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("_____________________________________________"));
 	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
 	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+	// ErrorMessage = TEXT("Server is full"); // ErrorMessage에 값이 입력되면 오류로 간주되어 접속이 차단됨. 하지만 STANDALONE으로 실행은 가능
+	
 	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
 }
 
@@ -39,6 +45,22 @@ void AABGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
 	Super::PostLogin(NewPlayer);
+	UNetDriver* NetDriver = GetNetDriver();
+	if (NetDriver)
+	{
+		if (NetDriver->ClientConnections.Num() == 0)
+		{
+			AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No ClientConnections"));
+		}
+		for (const auto& Connection: NetDriver->ClientConnections)
+		{
+			AB_LOG(LogABNetwork, Log, TEXT("Client Connection: %s"), *Connection->GetName());
+		}
+	}
+	else
+	{
+		AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No NetDriver"));
+	}
 	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
 }
 
