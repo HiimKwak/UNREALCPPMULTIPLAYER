299ed3f7f8e679a323afdffefc1f5ee30c8ab609
곽민규 <mindol0320@gmail.com>
2026-05-29 11:46:17 +0900

    리슨서버 로그인 로그 실습

---
+++ Source/ArenaBattle/ArenaBattle.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define LOG_NETMODEINFO(GetNetMode())
+
+// __FUNCTION__ -> C문자열로 함수 이름값 전달해줌, 언리얼은 C문자열 말고 wchar_t 문자열을 쓰기 때문에 ANSI_TO_TCHAR 함수로 변환해줘야함
+// __FILE__, __LINE__ 매크로도 있음. 이걸로 에러로그시스템 구체화 가능
+#define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
+
+#define AB_LOG(LogCat, Verbosity, Format, ...) \
+	UE_LOG(LogCat, Verbosity, TEXT("%s %s"), \
+		LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
