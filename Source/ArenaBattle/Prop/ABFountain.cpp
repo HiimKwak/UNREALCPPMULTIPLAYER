e4199d62f19251b357382f3292006a86fb7aa589
곽민규 <mindol0320@gmail.com>
2026-06-02 15:45:27 +0900

    전송 빈도 수 낮추기, 클라이언트에서 보간

--- Source/ArenaBattle/Prop/ABFountain.cpp
+++ Source/ArenaBattle/Prop/ABFountain.cpp
@@ -2,7 +2,10 @@
 
 
 #include "Prop/ABFountain.h"
+
+#include "ArenaBattle.h"
 #include "Components/StaticMeshComponent.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 AABFountain::AABFountain()
@@ -28,13 +31,45 @@ AABFountain::AABFountain()
 	{
 		Water->SetStaticMesh(WaterMeshRef.Object);
 	}
+	
+	// 1. 액터의 리플리케이션 속성 켜주기
+	bReplicates = true;
+	
+	NetUpdateFrequency = 1.f; // 네트워크 전송 빈도 1초에 1번
 }
 
 // Called when the game starts or when spawned
 void AABFountain::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AABFountain::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	
+	// 3. 네트웍으로 복제할 속성 DOREPLIFETIME 매크로로 명시
+	DOREPLIFETIME(AABFountain, ServerRotationYaw);
+}
+
+void AABFountain::OnActorChannelOpen(class FInBunch& InBunch, class UNetConnection* Connection)
+{
+	Super::OnActorChannelOpen(InBunch, Connection);
+	
+}
+
+void AABFountain::OnRep_ServerRotationYaw()
+{
+	FRotator NewRotator = RootComponent->GetComponentRotation();
+	NewRotator.Yaw = ServerRotationYaw;
+
+	RootComponent->SetWorldRotation(NewRotator);
+	
+	// 이전 서버의 업데이트로부터 이번 업데이트까지 걸린 시간 저장
+	ClientTimeBetweenLastUpdate = ClientTimeSinceUpdate;
+
+	// 서버로부터 데이터를 받으면 0으로 초기화
+	ClientTimeSinceUpdate = 0.0f;
 }
 
 // Called every frame
@@ -42,5 +77,39 @@ void AABFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (HasAuthority())
+	{
+		AddActorLocalRotation(FRotator(0.0f, RotationRate * DeltaTime, 0.0f));
+		ServerRotationYaw = RootComponent->GetComponentRotation().Yaw;
+	}
+	else
+	{
+		// 서버로부터 데이터를 받은 후에 경과한 시간 계산
+		ClientTimeSinceUpdate += DeltaTime;
+
+		// 너무 작은 시간이 경과했을 때는 의미가 없음
+		if (ClientTimeSinceUpdate < KINDA_SMALL_NUMBER)
+		{
+			return;
+		}
+
+		// 다음 네트워크 패킷 전송 때 전달될 회전 값 예측
+		const float EstimateRotationYaw
+			= ServerRotationYaw + RotationRate * ClientTimeBetweenLastUpdate;
+
+		// 보간할 비율(alpha) 구하기
+		const float LerpRatio
+			= ClientTimeSinceUpdate / ClientTimeBetweenLastUpdate;
+
+		// 보간(Lerp)
+		const float ClientNewYaw 
+			= FMath::Lerp(ServerRotationYaw, EstimateRotationYaw, LerpRatio);
+
+		// 회전 값 설정 및 적용
+		FRotator ClientRotator = RootComponent->GetComponentRotation();
+		ClientRotator.Yaw = ClientNewYaw;
+
+		RootComponent->SetWorldRotation(ClientRotator);
+	}
 }
 
--- Source/ArenaBattle/Prop/ABFountain.h
+++ Source/ArenaBattle/Prop/ABFountain.h
@@ -18,6 +18,22 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void OnActorChannelOpen(class FInBunch& InBunch, class UNetConnection* Connection) override;
+	// 리플리케이션 콜백 함수는 UFUNCTION으로 등록돼야 넘어감
+	UFUNCTION()
+	void OnRep_ServerRotationYaw();
+	// 2. 네트웍으로 복제할 액터의 속성 키워드로 지정
+	UPROPERTY(ReplicatedUsing = OnRep_ServerRotationYaw)
+	float ServerRotationYaw;
+	
+	float RotationRate = 30.f;
+	
+	// 서버로부터 패킷을 받은 후에 경과한 시간을 계산하기 위한 변수
+	float ClientTimeSinceUpdate = 0.0f;
+
+	// 서버로부터 데이터를 받고 그 다음 데이터를 받았을 때까지 걸린 시간을 기록할 변수
+	float ClientTimeBetweenLastUpdate = 0.0f;
 
 public:	
 	// Called every frame
